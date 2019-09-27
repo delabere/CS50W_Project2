64a97446373c242dcebb15e153bda57adf81849e
delabere <jack.rickards@hotmail.co.uk>
2019-09-27 17:45:11 +0100

    formatting

--- static/index.js
+++ static/index.js
@@ -1,13 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
 
-
-    document.addEventListener('DOMContentLoaded', () => {
-
-      // change title of chat to the selected element's innerhtml
-      document.querySelectorAll('#chat-selector').forEach((chat) => {
+    // change title of chat to the selected element's innerhtml
+    document.querySelectorAll('#chat-selector').forEach((chat) => {
         chat.onclick = () => {
-          title = chat.innerHTML;
-          document.querySelector('#chat-title').innerHTML = title;
+            document.querySelector('#chat-title').innerHTML = chat.innerHTML;
         };
-      });
-
     });
+
+});
