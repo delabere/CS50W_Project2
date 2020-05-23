6c0dd01ad8daf5f54703be001645a9fbac147ab8
Jack <jack.rickards@hotmail.co.uk>
2020-05-23 16:21:35 +0000

    changed running port to 9990

--- gif_chat/application.py
+++ gif_chat/application.py
@@ -79,4 +79,4 @@ def create_room(data):
 
 # # if not run like this then SocketIO error is raised
 if __name__ == "__main__":
-    socketio.run(app, host='0.0.0.0', port=80)
+    socketio.run(app, host='0.0.0.0', port=9990)
